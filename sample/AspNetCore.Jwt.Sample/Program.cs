93a1890a6c9aa3f26d19391d22f3fc963c4d6aaa
Saji Weerasingham <saji.weerasingham@outlook.com>
2018-06-03 08:09:25 +1000

    started creating sample api web app

---
+++ sample/AspNetCore.Jwt.Sample/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCore.Jwt.Sample
+{
+    /// <summary>
+    /// Application Program
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main method
+        /// </summary>
+        /// <param name="args">Command-line arguments</param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .UseStartup<Startup>();
+    }
+}
---
+++ sample/AspNetCore.Jwt.Sample/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore.Jwt.Sample
+{
+    /// <summary>
+    /// Startup
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class
+        /// </summary>
+        /// <param name="configuration">Application configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private IConfiguration Configuration { get; set; }
+
+        /// <summary>
+        /// Configures application services
+        /// </summary>
+        /// <param name="services">Services collection</param>
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        /// <summary>
+        /// Configures the HTTP request pipeline
+        /// </summary>
+        /// <param name="app">Application builder</param>
+        /// <param name="env">Hosting environment</param>
+        public static void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
